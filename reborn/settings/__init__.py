2246db1867f1047062256a4aaa3854f90a3ef4ab
zswj123 <l2se@sina.cn>
2020-05-29 01:37:59 +0800

    发布后，路径找不到。

--- reborn/settings/__init__.py
+++ reborn/settings/__init__.py
@@ -1,2 +1,4 @@
-SSL_KEYFILE = "./settings/server.key"
-SSL_CERTFILE = "./settings/server.crt"
+import os
+
+SSL_KEYFILE = os.path.split(os.path.realpath(__file__))[0] + os.sep + "server.key"
+SSL_CERTFILE = os.path.split(os.path.realpath(__file__))[0] + os.sep + "server.crt"
