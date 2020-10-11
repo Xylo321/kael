72ce44a300f9b8a598da7da1343e7cc0541fadfd
congshi <congshi.hello@gmail.com>
2020-10-11 17:08:56 +0800

    生产环境测试

--- reborn/apps/__init__.py
+++ reborn/apps/__init__.py
@@ -178,6 +178,7 @@ def main():
 
 if __name__ == '__main__':
     import sys
+    print(sys.argv)
     if sys.argv[1] == '0':
         logging.basicConfig(level=logging.ERROR)
         APP.run(host='0.0.0.0', port=8000, debug=True, ssl_context=(SSL_CERTFILE, SSL_KEYFILE))
