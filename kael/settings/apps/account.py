3d95d56b1447cd7fb6688c46f620891e7e9bfb3b
kael <congshi.hello@gmail.com>
2021-02-07 08:01:43 +0800

    开发环境上的邮箱密码

--- kael/settings/apps/account.py
+++ kael/settings/apps/account.py
@@ -15,7 +15,7 @@ MAIL_CONFIG = {
     'host': "smtp.qq.com",
     'port': 465,
     'username': '858556393@qq.com',
-    'password': 'hacnvlxplaqkbbhj',
+    'password': 'payzkmajwvjybfbg',
     'forget_password_msg': "%d"
 }
 
